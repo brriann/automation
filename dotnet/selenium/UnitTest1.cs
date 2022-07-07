8b9530cd12a1ebf7c7cc0793a6f91b44c95cdd34
Brian Foster <brriann@gmail.com>
2022-07-07 17:09:17 -0600

    dotnet selenium

--- dotnet/selenium/UnitTest1.cs
+++ dotnet/selenium/UnitTest1.cs
@@ -12,18 +12,18 @@ public class UnitTest1
     [TestMethod]
     public void TestMethod1()
     {
-        using (var driver = new ChromeDriver("/PATH/TO/CHROMEDRIVER"))
+        using (var driver = new ChromeDriver("/path/to/chromedriver"))
         {
-            driver.Navigate().GoToUrl("STARTING_URL");
+            driver.Navigate().GoToUrl("homePageUrl");
             WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
             IWebElement loginPageLink = wait.Until(e => e.FindElement(By.CssSelector("#loginLink")));
             loginPageLink.Click();
             
             IWebElement emailTextField = wait.Until(e => e.FindElement(By.CssSelector("#Email")));
-            emailTextField.SendKeys("TEST_EMAIL");
+            emailTextField.SendKeys("testUserEmail");
 
             IWebElement passwordTextField = wait.Until(e => e.FindElement(By.CssSelector("#Password")));
-            passwordTextField.SendKeys("TEST_PASSWORD");
+            passwordTextField.SendKeys("testUserPassword");
 
             IWebElement loginButton = wait.Until(e => e.FindElement(By.CssSelector("#loginForm .btn-default:nth-of-type(1)")));
             loginButton.Click();
---
+++ dotnet/selenium/readme.md
@@ -0,0 +1,9 @@
+# DOTNET SELENIUM PROJECT
+
+- website automation and tests
+
+ChromeDriver download
+
+dotnet build
+
+dotnet test
